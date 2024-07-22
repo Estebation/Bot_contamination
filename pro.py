0fcb08a000b0f0da1bd6f23fe1bee3bd3690f832
Estebation <150539344+Estebation@users.noreply.github.com>
2024-07-22 15:50:42 -0300

    Delete pro.py

--- pro.py
+++
@@ -1,9 +0,0 @@
-import random
-characters = ("+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-ch_input = int(input("Introduce la longitud de la contraseña"))
-password = ("")
-
-for i in range(ch_input):
-    password = password + random.choice(characters)
-
-print(password)
